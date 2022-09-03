7113acae9ce082cf9cf21c98b244c3e7ae87d2ee
Андрей Ростовцев <Electron4ik123@gmail.com>
2022-09-03 12:45:35 +0300

    add PostAdmin

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+# Из модуля models импортируем модель Post
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'  
+
+admin.site.register(Post, PostAdmin)
