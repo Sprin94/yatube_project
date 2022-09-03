606d85cc57d24273622bcf6563ab87b4ac4fd874
Андрей Ростовцев <Electron4ik123@gmail.com>
2022-09-03 20:25:27 +0300

    add group_post render template

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-from django.shortcuts import render
-
+from .models import Group, Post
 
 def index(request):
-    template = 'posts/index.html'
-    context = {}
-    context['text'] = 'Это главная страница'
-    return render(request, template, context)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    context = {}
-    context['text'] = 'Здесь будет информация о группах проекта Yatube'
-    return render(request, template, context)
+    group =  get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
