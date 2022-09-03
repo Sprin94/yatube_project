5c94c49a935b493c16f234a5813a3b55d04bed77
Андрей Ростовцев <Electron4ik123@gmail.com>
2022-09-03 11:19:18 +0300

    add namespace and name

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,13 +1,13 @@
 # posts/urls.py
 from django.urls import path
 
-
 from . import views
 
+app_name = 'posts'
 
 urlpatterns = [
     #главная страница
-    path('', views.index),
+    path('', views.index, name='index'),
     #посты группы
-    path('groups/<slug:slug>/', views.group_posts),
+    path('groups/<slug:slug>/', views.group_posts, name='group_posts'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,8 +5,12 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    context = {}
+    context['text'] = 'Это главная страница'
+    return render(request, template, context)
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    context = {}
+    context['text'] = 'Здесь будет информация о группах проекта Yatube'
+    return render(request, template, context)
